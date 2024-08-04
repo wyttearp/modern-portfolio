6f8fe363eed97bd4098b94344c8e0e21fd6ad124
Wyatt LeVine <wyttearp@gmail.com>
2024-08-04 12:29:45 -0400

    Updates

---
+++ precache-manifest.e9b24cb438a59b66718ecfea85ccc52a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf117eaa33f6386078be56264eb425a",
+    "url": "/modern-portfolio/index.html"
+  },
+  {
+    "revision": "5ec3e40c6dc2f2591f4e",
+    "url": "/modern-portfolio/static/css/main.df273849.chunk.css"
+  },
+  {
+    "revision": "3e725b9428bec5cc59b0",
+    "url": "/modern-portfolio/static/js/2.9e0238e0.chunk.js"
+  },
+  {
+    "revision": "5ec3e40c6dc2f2591f4e",
+    "url": "/modern-portfolio/static/js/main.4dbe204b.chunk.js"
+  },
+  {
+    "revision": "07f71d20686964b92431",
+    "url": "/modern-portfolio/static/js/runtime~main.534d0616.js"
+  }
+]);
