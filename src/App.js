fa7406b64e2da7b0770fb765e9e4c2814a3cc063
Wyatt LeVine <wyttearp@gmail.com>
2024-08-04 16:16:49 -0400

    Updated routes and added dynamic year in footer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import ProjectGrid from './components/ProjectGrid/ProjectGrid';
 import Skills from './components/Skills/Skills';
+import Contact from './components/Contact/Contact';
 import DesignSection from './components/SkillsSections/DesignSection';
 import DevelopmentSection from './components/SkillsSections/DevelopmentSection';
 import AnimationSection from './components/SkillsSections/AnimationSection';
 import VideoSection from './components/SkillsSections/VideoSection';
-import Contact from './components/Contact/Contact';
-import './App.css';
 
 function App() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <Router>
       <div className="App">
@@ -33,7 +34,7 @@ function App() {
           </Routes>
         </main>
         <footer>
-          <p>&copy; 2024 Your Name. All rights reserved.</p>
+          <p>&copy; {currentYear} Your Name. All rights reserved.</p>
         </footer>
       </div>
     </Router>
